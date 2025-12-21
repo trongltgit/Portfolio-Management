eb1b72f5a467bb44f1e6ef427a7c04d65d83967f
trongltgit <tronglt.hcm@gmail.com>
2025-12-21 23:41:40 +0700

    Update __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,11 @@
-# market/__init__.py
 from flask import Blueprint
 
 market_bp = Blueprint(
     "market",
     __name__,
+    template_folder="templates",
     url_prefix="/market"
 )
 
+# import routes cuối cùng để tránh circular import
 from . import routes
